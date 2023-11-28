fe47a57d22e5f57b7c399b58c77aab3dc3274c02
Alexandre Leroux <alex@sherpa.one>
2023-11-28 21:55:58 +0100

    first commit

---
+++ tests/math.test.ts
@@ -0,0 +1,8 @@
+import { describe, expect, test } from "@jest/globals";
+beforeEach(() => {});
+
+describe("Math", () => {
+  test("1 + 1 should equal 2", () => {
+    expect(1 + 1).toEqual(2);
+  });
+});
